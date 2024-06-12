526252f97b7ba9f0b288a4bc0cddcff069be709c
housten <14905544+housten@users.noreply.github.com>
2024-06-12 11:31:34 +0200

    remove duplicate test from wrong place

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,8 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from './components/TodoList';
-import TodoForm from './components/TodoForm';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const titleElement = screen.getByText(/TODO thingy/i);
   expect(titleElement).toBeInTheDocument();
-});
-test('allows the user to add a new todo', () => {
-  render(<App />);
-  const inputElement = screen.getByPlaceholderText(/Add a new todo/i);
-  const buttonElement = screen.getByText(/Add/i);
-
-  fireEvent.change(inputElement, { target: { value: 'New Todo' } });
-  fireEvent.click(buttonElement);
-
-  const newTodoElement = screen.getByText(/New Todo/i);
-  expect(newTodoElement).toBeInTheDocument();
 });
